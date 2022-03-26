bdfa9c8136d69ac83a2278841d09dc2d8a1fa04d
mqtt-halogenki <magda.pychtjarow@gmail.com>
2022-03-26 09:34:40 +0100

    Dql, experimental set up

---
+++ DQL.py
@@ -0,0 +1,196 @@
+import random
+import gym
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.optimizers import Adam
+from matplotlib import pyplot as plt
+from collections import deque
+import sys
+
+def DqnModel(input_shape, action_space,learning_rate):
+    input_layer = Input(input_shape)
+    layer = Dense(24, input_shape=input_shape, activation="relu")(input_layer)
+    layer = Dense(16, activation="relu")(layer)
+    #layer = Dense(64, activation="relu")(layer)
+    layer = Dense(action_space, activation="linear")(layer)
+    model = Model(inputs=input_layer, outputs=layer)
+    model.compile(loss="mse", optimizer=Adam(learning_rate), metrics=["accuracy"])
+    return model
+
+
+class DQNAgent:
+    def __init__(self,n_states, n_actions, memory_buffer_size,learning_rate,epsilon,gamma,batch_size, with_memory = True, with_tn = True):
+        self.state_size = n_states
+        self.action_size = n_actions
+
+        self.target_updates = 0
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.target_updates_treshold = 10
+
+        self.model = DqnModel(input_shape=(self.state_size,), action_space=self.action_size,learning_rate=self.learning_rate)
+
+        if with_memory:
+            self.memory = deque(maxlen=memory_buffer_size)
+
+        if with_tn:
+            self.target_network = DqnModel(input_shape=(self.state_size,), action_space=self.action_size, learning_rate=self.learning_rate)
+            self.target_network.set_weights(self.model.get_weights())
+
+
+    def update_buffer(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def select_action(self, state):
+        if np.random.random() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            return np.argmax(self.model.predict((np.reshape(state, [1,4]))))
+
+
+    def train(self,terminalstate,transition, with_tn, with_mb):
+        if with_mb:
+
+            minibatch = random.sample(self.memory, self.batch_size)
+            obses_t = [x[0] for x in minibatch]
+            actions = [x[1] for x in minibatch]
+            rewards = [x[2] for x in minibatch]
+            obses_tp1 = [x[3] for x in minibatch]
+            dones = [x[4] for x in minibatch]
+
+
+            current_qs = self.model.predict(np.array(obses_t),batch_size = self.batch_size)
+        else:
+            obses_t, action, reward, obses_tp1, done = transition
+            current_qs = self.model.predict((np.reshape(obses_t, [1,4])))
+
+        if with_tn:
+            if with_mb:
+                future_qs = self.target_network.predict((np.array(obses_tp1))) #more stability
+            else:
+                future_qs = self.target_network.predict((np.reshape(obses_tp1, [1, 4])))
+
+        else:
+            if with_mb:
+                future_qs = self.model.predict((np.array(obses_tp1)))
+            else:
+                future_qs = self.model.predict((np.reshape(obses_tp1, [1, 4])))
+
+        if with_mb:
+            for i in range(self.batch_size):
+                if dones[i]:
+                    current_qs[i][actions[i]] = rewards[i]
+                else:
+                    current_qs[i][actions[i]] = rewards[i] + self.gamma * (np.amax(future_qs[i]))
+
+            self.model.fit(np.array(obses_t), current_qs, batch_size=self.batch_size, verbose=0)
+
+        else:
+            if done:
+                current_qs[0][action] = reward
+            else:
+                current_qs[0][action] = reward + self.gamma * (np.amax(future_qs))
+
+            self.model.fit((np.reshape(obses_tp1, [1, 4])), (np.reshape(current_qs, [1, 2])), batch_size=self.batch_size, verbose=0)
+
+        if not terminalstate:
+            self.target_updates += 1
+
+        if with_tn:
+            if self.target_updates > self.target_updates_treshold:
+                self.target_network.set_weights(self.model.get_weights())
+                self.target_updates = 0
+
+
+
+def deep_q_learning(n_episodes,max_episode_lenght,memory_buffer_size,learning_rate,gamma,epsilon,batch_size,with_mb = True, with_tn = True):
+    env = gym.make('CartPole-v1')
+    env.reset()
+    DQL_Agent = DQNAgent(env.observation_space.shape[0], env.action_space.n,memory_buffer_size, learning_rate,epsilon, gamma,batch_size,with_memory=with_mb,with_tn=with_tn)
+    rewards = []
+    episodes_left = n_episodes
+    for episode in range(n_episodes):
+        print(episodes_left)
+        state = env.reset()
+        done = False
+        for i in range(max_episode_lenght):
+            #env.render()
+            action = DQL_Agent.select_action(state)
+            next_state, reward, done, _ = env.step(action)
+
+            if done:
+                rewards.append(i+1)
+                episodes_left-=1
+                break
+
+            if with_mb:
+                DQL_Agent.update_buffer(state, action, reward, next_state, done)
+
+            state = next_state
+            transition = (state, action, reward, next_state, done)
+
+            if with_mb:
+                if len(DQL_Agent.memory) > batch_size:
+                    DQL_Agent.train(done,transition,with_tn=with_tn, with_mb=with_mb)
+            else:
+                DQL_Agent.train(done, transition, with_tn=with_tn, with_mb=with_mb)
+
+    env.close()
+    return rewards
+
+def main(argv):
+    with_tn = False
+    with_mb = False
+
+    if len(sys.argv) > 0:
+        if "target_network" in sys.argv:
+            with_tn = True
+        if "experience_buffer" in sys.argv:
+            with_mb = True
+
+    print("Deep Q learning: target_network: {}, experience buffer {}".format(with_tn, with_mb))
+    n_episodes = 200
+    memory_buffer_size = 2000
+    gamma = 0.95
+    learning_rate = 0.01
+    max_episode_lenght = 200
+    epsilon = 0.2
+    batch_size = 32
+
+    rewards = deep_q_learning(n_episodes=n_episodes,max_episode_lenght=max_episode_lenght,memory_buffer_size=memory_buffer_size,
+                              learning_rate=learning_rate, gamma=gamma, epsilon=epsilon,batch_size=batch_size, with_mb=with_mb,with_tn=with_tn )
+
+    x = range(0, len(rewards))
+    fig, ax = plt.subplots()
+    ax.plot(x,rewards)
+
+    z = np.polyfit(x, rewards, 1)
+    p = np.poly1d(z)
+    plt.plot(x, p(x), "r--")
+
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.show()
+
+
+    title = "Deep Q learning performance"
+    if with_tn or with_mb:
+        title += " with "
+        if with_tn:
+            title+= "Target Network "
+        if with_tn and with_mb:
+            title += "and "
+        if with_mb:
+            title+= "Experience Buffer "
+
+    plt.title(title)
+
+    print("Obtained rewards: {}".format(rewards))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
---
+++ Evaluation.py
@@ -0,0 +1,90 @@
+import numpy as np
+import time
+from DQL import deep_q_learning
+from scipy.signal import savgol_filter
+import matplotlib.pyplot as plt
+
+
+class LearningCurvePlot:
+
+    def __init__(self, title="Experiment"):
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlabel('Episode')
+        self.ax.set_ylabel('Duration')
+        if title is not None:
+            self.ax.set_title(title)
+
+    def add_curve(self, y, label=None):
+        if label is not None:
+            self.ax.plot(y, label=label)
+        else:
+            self.ax.plot(y)
+
+    def set_ylim(self, lower, upper):
+        self.ax.set_ylim([lower, upper])
+
+    def save(self, name):
+        self.ax.legend()
+        self.fig.savefig(name, dpi=300)
+
+
+
+def smooth(y, window, poly=1):
+    return savgol_filter(y,window,poly)
+
+
+def average_over_repetitions(n_repetitions,n_episodes,smoothing_widnow,with_mb=True,with_tn=True):
+    reward_results = np.empty([n_repetitions, n_episodes])  # Result array
+
+
+    for rep in range(n_repetitions):
+        #     rewards = deep_q_learning(n_episodes=n_episodes,max_episode_lenght=max_episode_lenght,memory_buffer_size=memory_buffer_size, learning_rate=learning_rate, gamma=gamma, epsilon=epsilon,batch_size=64, with_mb=True,with_tn=True )
+        rewards = deep_q_learning(n_episodes=n_episodes,max_episode_lenght=200,memory_buffer_size=2000,learning_rate=0.1,gamma=1.0,epsilon=0.01,batch_size=32,with_mb=with_mb,with_tn=with_tn)
+        reward_results[rep] = rewards
+
+    learning_curve = np.mean(reward_results, axis=0)  # average over repetitions
+    learning_curve = smooth(learning_curve, smoothing_widnow)  # additional smoothing
+    return learning_curve
+
+def experiment():
+    n_episodes = 200
+    Plot = LearningCurvePlot()
+    learning_curve = average_over_repetitions(10, n_episodes,31,with_mb=True,with_tn=True)
+    Plot.add_curve(learning_curve)
+    Plot.save("network_learning.png")
+
+def experiment_lr():
+    n_episodes = 200
+    learning_rates = [0.01, 0.2, 0.5]
+    Plot = LearningCurvePlot()
+    for lr in learning_rates:
+        learning_curve = average_over_repetitions(50, n_episodes)
+        Plot.add_curve(learning_curve,label=r'Learning rate = {}'.format(lr))
+    Plot.save("learning_rates.png")
+def experiment_eb():
+    n_episodes = 200
+    smoothing_window = 31
+    Plot = LearningCurvePlot()
+    now = time.time()
+    without = average_over_repetitions(1,n_episodes,smoothing_window,with_mb=False,with_tn=False)
+    print('Without Experience Buffer and Target Network: {} minutes'.format((time.time() - now) / 60))
+    now = time.time()
+    with_eb_learning_curve = average_over_repetitions(1,n_episodes,smoothing_window,with_mb=True,with_tn=False)
+    print('With Experience Buffer: {} minutes'.format((time.time() - now) / 60))
+    now = time.time()
+    with_tn_learning_curve = average_over_repetitions(1,n_episodes,smoothing_window,with_mb=False,with_tn=True)
+    print('With Target Network: {} minutes'.format((time.time() - now) / 60))
+    now = time.time()
+    with_tn__eb_learning_curve = average_over_repetitions(1,n_episodes,smoothing_window, with_mb=True, with_tn=True)
+    print('With Target Network and Experience Buffer: {} minutes'.format((time.time() - now) / 60))
+
+    Plot.add_curve(without,label=r'Deep Q learning without Replay Buffer and Target Network')
+    Plot.add_curve(with_eb_learning_curve,label=r'Deep Q learning with Replay Buffer')
+    Plot.add_curve(with_tn__eb_learning_curve,label=r'Deep Q learning with Replay Buffer and Target Network')
+    Plot.add_curve(with_tn_learning_curve,label=r'Deep Q learning with Target Network')
+
+    Plot.save("different_elements.png")
+
+experiment()
+#experiment_lr()
+#experiment_eb()
